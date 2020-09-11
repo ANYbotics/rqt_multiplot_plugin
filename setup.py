a87f41312a27ac24e224dc15024e1074bec990a8
Philipp Leemann <pleemann@anybotics.com>
2020-09-11 18:03:29 +0000

    Merge branch 'feature/noetic' into 'master'
    Feature/noetic
    
    Closes #UNKNOWN and #UNKNOWN
    
    GitOrigin-RevId: 960d8e58290245ef1cf393dde684cc786d01752d

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-from distutils.core import setup
+#!/usr/bin/env python3
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
 packages=['rqt_multiplot'],
